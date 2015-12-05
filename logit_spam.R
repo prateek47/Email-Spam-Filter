79c70ec139ec2cd6b8e715e809bbfff7a2b26c50
Paul <paulchrn@gmail.com>
2015-12-05 14:49:23 -0500

    modified data cleaning part

--- logit_spam.R
+++ logit_spam.R
@@ -30,29 +30,26 @@ ham.emails <- sapply(dir(ham.data), function(x){
   })
 #5052
 
+spam.emails=sapply(spam.emails,function(x) gsub('<[^>]*>','',x))
+spam.emails=sapply(spam.emails,function(x) gsub('\\n','',x))
+
+ham.emails=sapply(ham.emails,function(x) gsub('<[^>]*>','',x))
+ham.emails=sapply(ham.emails,function(x) gsub('\\n','',x))
+
 ## dividing into train and test data
-# keeping a spam ratio of arnd 30% in both training and test data
-spam.ind=sample(1:length(spam.emails),1200)
-spam.train=spam.emails[spam.ind]
-spam.test=spam.emails[-spam.ind]
+# keeping a spam ratio of arnd 20% in both training and test data
+
+spam.emails.mod=spam.emails[sample(1:length(spam.emails),length(spam.emails)*2/3)]
+
+# dividing into trainng and test sets in the ratio 3:1
+spam.ind=sample(1:length(spam.emails.mod),ceiling(length(spam.emails.mod)*3/4))
+spam.train=spam.emails.mod[spam.ind]
+spam.test=spam.emails.mod[-spam.ind]
 # 698
 
-ham.ind=sample(1:length(ham.emails),2500)
+ham.ind=sample(1:length(ham.emails),ceiling(length(ham.emails)*3/4))
 ham.train=ham.emails[ham.ind]
-ham.test=ham.emails[-ham.ind][1:1700]
-
-## text cleaning
-#remove html tags
-spam.train=sapply(spam.train,function(x) gsub('<[^>]*>','',x))
-spam.test=sapply(spam.test,function(x) gsub('<[^>]*>','',x))
-ham.train=sapply(ham.train,function(x) gsub('<[^>]*>','',x))
-ham.test=sapply(ham.test,function(x) gsub('<[^>]*>','',x))
-#remove line break
-spam.train=sapply(spam.train,function(x) gsub('(\\[a-z]){1}','',x))
-ham.train=sapply(ham.train,function(x) gsub('\\n','',x))
-spam.test=sapply(spam.test,function(x) gsub('\\n','',x))
-ham.test=sapply(ham.test,function(x) gsub('\\n','',x))
-
+ham.test=ham.emails[-ham.ind]
 
 # creating the corpus for spams
 create.corpus <- function(data)
@@ -103,7 +100,7 @@ return (data.df)
 
 spam.df=generate.probs(spam.corp)
 
-# creating the corpus for spams
+# creating the corpus for hams
 ham.corp <- create.corpus(ham.train)
 
 #generate probs for ham
